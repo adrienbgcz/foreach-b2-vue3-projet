0b3d2b8070239df4c78384465c136b40edcb155c
Adrien Bogacz <ad.bogacz@gmail.com>
2022-04-14 11:29:02 +0200

    :package: add autoimport

--- adrien-bogacz/adrien-bogacz-cv/src/pages/Home.vue
+++ adrien-bogacz/adrien-bogacz-cv/src/pages/Home.vue
@@ -4,7 +4,7 @@ import { useRoute, useRouter } from 'vue-router'
 const router = useRouter()
 const route = useRoute()
 
-
+onMounted(() => console.log(route.fullPath))
 </script>
 
 <template>
--- adrien-bogacz/adrien-bogacz-cv/vite.config.ts
+++ adrien-bogacz/adrien-bogacz-cv/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pages from 'vite-plugin-pages'
+import autoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), pages()]
+  plugins: [vue(), pages(), autoImport({ imports: ['vue', 'vue-i18n', 'vue-router'], dts: 'src/auto-imports.d.ts' })]
 })
