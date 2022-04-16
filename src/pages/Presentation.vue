6736cc9b1aac60405ba488873ff3875ef83ca9ca
Adrien Bogacz <ad.bogacz@gmail.com>
2022-04-16 11:27:01 +0200

    :sparkles: create navBar (plugin animate seems doesn't work)

---
+++ src/pages/Presentation.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import NavBar from '../components/navBar.vue'
+const { t } = useI18n()
+
+const route = useRoute()
+
+onMounted(() => console.log(route.fullPath))
+
+</script>
+
+<template class="bg-gray-500">
+
+  <div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio eu feugiat pretium nibh. Ullamcorper velit sed ullamcorper morbi tincidunt ornare. Mi sit amet mauris commodo. Sit amet purus gravida quis blandit turpis cursus in. Odio euismod lacinia at quis. Enim nunc faucibus a pellentesque sit amet porttitor eget dolor. Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus. Semper quis lectus nulla at. Parturient montes nascetur ridiculus mus mauris. Id neque aliquam vestibulum morbi blandit cursus risus at. Auctor urna nunc id cursus. Velit euismod in pellentesque massa placerat. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing. Rhoncus urna neque viverra justo nec ultrices.</p>
+
+    <p>At elementum eu facilisis sed odio morbi quis. Amet aliquam id diam maecenas. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Sed enim ut sem viverra. Euismod elementum nisi quis eleifend quam adipiscing vitae. Facilisis mauris sit amet massa. Posuere urna nec tincidunt praesent. Quisque sagittis purus sit amet volutpat consequat mauris nunc. Dictum non consectetur a erat nam. Etiam erat velit scelerisque in. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. Risus ultricies tristique nulla aliquet enim tortor. Dignissim suspendisse in est ante. Etiam sit amet nisl purus in mollis nunc sed. Quis varius quam quisque id diam vel quam elementum pulvinar. Non nisi est sit amet facilisis magna. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Nunc sed velit dignissim sodales.</p>
+  </div>
+</template>
+
